9a51f16c2b9edd6ed52ed3e6fa826e81cf8fe7c8
lhuhu <lhuhulive@outlook.com>
2018-12-23 20:48:41 +0800

    加0

--- src/com/atguigu/gittest/DuGuJiuJian.java
+++ src/com/atguigu/gittest/DuGuJiuJian.java
@@ -3,6 +3,6 @@ package com.atguigu.gittest;
 public class DuGuJiuJian {
 	public static void main(String[] args) {
 		System.out.println("¶À¹Â¾Å½£");
-		System.out.println("123456789");
+		System.out.println("1234567890");
 	}
 }

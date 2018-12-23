95c422a9183544d39cf60515189966f949464916
lhuhu <lhuhulive@outlook.com>
2018-12-23 20:15:53 +0800

    第三次写入方法和输出语句

--- src/com/atguigu/gittest/DuGuJiuJian.java
+++ src/com/atguigu/gittest/DuGuJiuJian.java
@@ -1,5 +1,7 @@
 package com.atguigu.gittest;
 
 public class DuGuJiuJian {
-
+	public static void main(String[] args) {
+		System.out.println("¶À¹Â¾Å½£");
+	}
 }
--- src/com/atguigu/gittest/XiXingDaFa.java
+++ src/com/atguigu/gittest/XiXingDaFa.java
@@ -1,5 +1,7 @@
 package com.atguigu.gittest;
 
 public class XiXingDaFa {
-
+	public static void main(String[] args) {
+		System.out.println("ÎüĞÇ´ó·¨");
+	}
 }
